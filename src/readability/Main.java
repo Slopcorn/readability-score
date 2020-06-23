ac5888012e48f3a28b72172c22a71b755d71d5c1
Slopcorn <raido.everest@protonmail.ch>
2020-06-24 00:49:25 +0300

    Added method to calculate ARI

--- src/readability/Main.java
+++ src/readability/Main.java
@@ -22,4 +22,14 @@ public class Main {
         System.out.println(text);
         System.out.println(words / sents > 10 ? "HARD" : "EASY");
     }
+
+    /** Calculates the ARI.
+     * @param chars The amount of characters in the text, not counting whitespace.
+     * @param words The amount of words in the text.
+     * @param sents The amount of sentences in the text.
+     * @return The automated readability index of the text.
+     */
+    private double automatedReadabilityIndex(int chars, int words, int sents) {
+        return 4.71 * chars / words + 0.5 * words / sents - 21.43;
+    }
 }
