16569b58d3f7c8ad755435b924687e798e6e64bc
Slopcorn <raido.everest@protonmail.ch>
2020-06-24 10:59:10 +0300

    Rewritten to read text file, path as command line argument

--- src/readability/Main.java
+++ src/readability/Main.java
@@ -1,12 +1,35 @@
 package readability;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String text = scanner.nextLine();
+        if (args.length != 1) System.out.println("Please call the program with path to the input file.");
+        else try (Scanner scanner = new Scanner(new File(args[0]))) {
+            String text = getFileText(scanner);
+            printTextInformation(text);
+        } catch (FileNotFoundException e) {
+            System.out.println("Invalid filepath: " + args[0]);
+        }
+    }
 
+    /** Calculates the ARI.
+     * @param chars The amount of characters in the text, not counting whitespace.
+     * @param words The amount of words in the text.
+     * @param sents The amount of sentences in the text.
+     * @return The automated readability index of the text.
+     */
+    private double automatedReadabilityIndex(int chars, int words, int sents) {
+        return 4.71 * chars / words + 0.5 * words / sents - 21.43;
+    }
+
+    /** TODO: Gets and prints relevant information about input text.
+     * @param text Input text string.
+     */
+    private static void printTextInformation(String text) {
+        // update me
         // Split to find all the sentences in the text.
         String[] sentences = text.split("[.!?]");
 
@@ -23,13 +46,12 @@ public class Main {
         System.out.println(words / sents > 10 ? "HARD" : "EASY");
     }
 
-    /** Calculates the ARI.
-     * @param chars The amount of characters in the text, not counting whitespace.
-     * @param words The amount of words in the text.
-     * @param sents The amount of sentences in the text.
-     * @return The automated readability index of the text.
+    /** TODO: Gets all of the text from a scanner.
+     * @param scanner Input scanner.
+     * @return A string containing all of the text.
      */
-    private double automatedReadabilityIndex(int chars, int words, int sents) {
-        return 4.71 * chars / words + 0.5 * words / sents - 21.43;
+    private static String getFileText(Scanner scanner) {
+        // update me
+        return scanner.nextLine();
     }
 }
